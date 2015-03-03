8f4339ea348ced95411ded3afc43cd5e9b9ec53d
Stephen McDonald <steve@dancingbluesofa.com>
2015-03-03 17:53:35 +0000

    Added the SMS Sender

---
+++ SOS-Rest-Service/src/main/java/com/sagepay/sos/aws/SmsSender.java
@@ -0,0 +1,27 @@
+package com.sagepay.sos.aws;
+
+import com.clockworksms.ClockWorkSmsService;
+import com.clockworksms.ClockworkException;
+import com.clockworksms.ClockworkSmsResult;
+import com.clockworksms.SMS;
+
+public class SmsSender {
+
+    private static String SMS_API_KEY = "9ba976a2a87a01d2ea4d76238f7c906a1a19966d";
+
+    public static void sendSMS(String recipient, String body) {
+        try {
+            ClockWorkSmsService clockWorkSmsService = new ClockWorkSmsService(SMS_API_KEY);
+            SMS sms = new SMS(recipient, body);
+            ClockworkSmsResult result = clockWorkSmsService.send(sms);
+
+            if (result.isSuccess()) {
+                System.out.println("Sent with ID: " + result.getId());
+            } else {
+                System.out.println("Error: " + result.getErrorMessage());
+            }
+        } catch (ClockworkException e) {
+            e.printStackTrace();
+        }
+    }
+}
