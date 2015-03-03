5e8ec42b4f4b07dea563767ad9637006bdcfb2cd
ChossAddict <choss@outlook.com>
2015-03-03 18:14:40 +0000

    Ensuring chaser resource consumes JSON

--- SOS-Rest-Service/src/main/java/com/sagepay/sos/resources/ChaserResource.java
+++ SOS-Rest-Service/src/main/java/com/sagepay/sos/resources/ChaserResource.java
@@ -14,6 +14,7 @@ import javax.ws.rs.core.MediaType;
 
 @Path("/chasers")
 @Produces (MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
 public class ChaserResource {
 
     private final CancellationsDAO cancellationsDao;
