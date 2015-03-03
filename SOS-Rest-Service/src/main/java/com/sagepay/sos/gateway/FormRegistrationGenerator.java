b3fd36e58218bd6f036fc8743cfb5b1351e35f54
ChossAddict <choss@outlook.com>
2015-03-03 23:18:09 +0000

    Can generate a form registration string

--- SOS-Rest-Service/src/main/java/com/sagepay/sos/gateway/FormRegistrationGenerator.java
+++ SOS-Rest-Service/src/main/java/com/sagepay/sos/gateway/FormRegistrationGenerator.java
@@ -5,4 +5,19 @@ package com.sagepay.sos.gateway;
  */
 public class FormRegistrationGenerator {
 
+    private String vendor;
+    private String amount;
+
+    public FormRegistrationGenerator(String vendor, String amount) {
+        this.vendor = vendor;
+        this.amount = amount;
+    }
+
+    public String generateRegistrationString(String randomTxCodePart) {
+        return "VendorTxCode=chipbuttysrus-1386062" + randomTxCodePart + "&Amount=" + amount + "&Currency=GBP&Description=TESTPAYMENT&SuccessURL=http://www.google.com/sagepay-java-kit-1.2.2.0/form/success/&FailureURL=http://www.google.com/sagepay-java-kit-1.2.2.0/form/failure/" +
+                "&BillingSurname=Surname&BillingFirstnames=Fname Mname&BillingAddress1=BillAddress Line 1&BillingCity=BillCity&BillingPostCode=W1A 1BL&BillingCountry=GB&DeliverySurname=Surname&DeliveryFirstnames=Fname Mname&DeliveryAddress1=BillAddress Line 1" +
+                "&DeliveryCity=BillCity&DeliveryPostCode=W1A 1BL&DeliveryCountry=GB&CustomerName=Fname Mname Surname&CustomerEMail=customer@example.com&SendEMail=1&eMailMessage=Thanks for your order&BillingAddress2=BillAddress Line 2&BillingPhone=44 (0)7933 000 000" +
+                "&DeliveryAddress2=BillAddress Line 2&DeliveryPhone=44 (0)7933 000 000&AllowGiftAid=0&ApplyAVSCV2=0&Apply3DSecure=0";
+
+    }
 }
--- SOS-Rest-Service/src/test/java/com/sagepay/sos/WhenGeneratingFormRegistrationLink.java
+++ SOS-Rest-Service/src/test/java/com/sagepay/sos/WhenGeneratingFormRegistrationLink.java
@@ -18,4 +18,18 @@ public class WhenGeneratingFormRegistrationLink {
 
         assertEquals(expectedCrypt, actualCrypt);
     }
+
+    @Test
+    public void canCreateAFullFormRegistrationGivenRequiredFields() {
+        String vendor = "chipbuttysrus";
+        String amount = "22.22";
+        String randomTxCodePart = "555555";
+
+        String actualRegistration = new FormRegistrationGenerator(vendor, amount).
+                generateRegistrationString(randomTxCodePart);
+
+        String expectedRegistration = "VendorTxCode=chipbuttysrus-1386062555555&Amount=22.22&Currency=GBP&Description=TESTPAYMENT&SuccessURL=http://www.google.com/sagepay-java-kit-1.2.2.0/form/success/&FailureURL=http://www.google.com/sagepay-java-kit-1.2.2.0/form/failure/&BillingSurname=Surname&BillingFirstnames=Fname Mname&BillingAddress1=BillAddress Line 1&BillingCity=BillCity&BillingPostCode=W1A 1BL&BillingCountry=GB&DeliverySurname=Surname&DeliveryFirstnames=Fname Mname&DeliveryAddress1=BillAddress Line 1&DeliveryCity=BillCity&DeliveryPostCode=W1A 1BL&DeliveryCountry=GB&CustomerName=Fname Mname Surname&CustomerEMail=customer@example.com&SendEMail=1&eMailMessage=Thanks for your order&BillingAddress2=BillAddress Line 2&BillingPhone=44 (0)7933 000 000&DeliveryAddress2=BillAddress Line 2&DeliveryPhone=44 (0)7933 000 000&AllowGiftAid=0&ApplyAVSCV2=0&Apply3DSecure=0";
+
+        assertEquals(expectedRegistration, actualRegistration);
+    }
 }
