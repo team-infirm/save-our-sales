ea4dcd03bc39249a74005985c89adefd6ac3dca9
Stephen McDonald <steve@dancingbluesofa.com>
2015-03-03 14:10:11 +0000

    Wired up the static resources, and moved the rest endpoint to /api/

--- SOS-Rest-Service/src/main/java/com/sagepay/sos/TransactionApplication.java
+++ SOS-Rest-Service/src/main/java/com/sagepay/sos/TransactionApplication.java
@@ -6,13 +6,14 @@ import com.sagepay.sos.resources.CancellationResource;
 import com.sagepay.sos.resources.CancellationsResource;
 import com.sagepay.sos.resources.ChaserResource;
 import io.dropwizard.Application;
+import io.dropwizard.assets.AssetsBundle;
 import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
 
 public class TransactionApplication extends Application<TransactionApplicationConfiguration> {
     @Override
     public void initialize(Bootstrap<TransactionApplicationConfiguration> bootstrap) {
-
+        bootstrap.addBundle(new AssetsBundle("/assets", "/", "cancellation.html"));
     }
 
     public static void main(String[] args) throws Exception {
@@ -26,5 +27,6 @@ public class TransactionApplication extends Application<TransactionApplicationCo
         environment.jersey().register(new ChaserResource());
         environment.jersey().register(new CancellationResource(cancellationsDAO));
         environment.jersey().register(new CancellationsResource(cancellationsDAO));
+        environment.jersey().setUrlPattern("/api/*");
     }
 }
