2f5aca1bd48ab786dbc6e161e6f425219407b977
ChossAddict <choss@outlook.com>
2015-03-04 09:47:08 +0000

    Can create full registration link

--- SOS-Rest-Service/src/main/java/com/sagepay/sos/gateway/FormRegistrationGenerator.java
+++ SOS-Rest-Service/src/main/java/com/sagepay/sos/gateway/FormRegistrationGenerator.java
@@ -5,6 +5,9 @@ package com.sagepay.sos.gateway;
  */
 public class FormRegistrationGenerator {
 
+    public static final String FORM_REGISTRATION_URL = "https://test.sagepay.com/gateway/service/vspform-register.vsp";
+    public static final String QUERY_STRING_BEGIN = "?";
+    private static final String DELIMITER = "&";
     private String vendor;
     private String amount;
 
@@ -19,5 +22,17 @@ public class FormRegistrationGenerator {
                 "&DeliveryCity=BillCity&DeliveryPostCode=W1A 1BL&DeliveryCountry=GB&CustomerName=Fname Mname Surname&CustomerEMail=customer@example.com&SendEMail=1&eMailMessage=Thanks for your order&BillingAddress2=BillAddress Line 2&BillingPhone=44 (0)7933 000 000" +
                 "&DeliveryAddress2=BillAddress Line 2&DeliveryPhone=44 (0)7933 000 000&AllowGiftAid=0&ApplyAVSCV2=0&Apply3DSecure=0";
 
+    }
+
+    public static String createRegistrationLink(String cryptString, String vendor) {
+        StringBuilder linkBuilder = new StringBuilder();
+        linkBuilder.append(FORM_REGISTRATION_URL);
+        linkBuilder.append(QUERY_STRING_BEGIN);
+        linkBuilder.append("VPSProtocol=3.00" + DELIMITER);
+        linkBuilder.append("TxType=PAYMENT" + DELIMITER);
+        linkBuilder.append("Vendor=" + vendor + DELIMITER);
+        linkBuilder.append("Crypt=" + cryptString);
+
+        return linkBuilder.toString();
     }
 }
