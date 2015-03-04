64d6ed86b00e83787eba05c3ada904fda1a32af3
Stephen McDonald <steve@dancingbluesofa.com>
2015-03-04 08:45:52 +0000

    Added parsing of the decrypted crypt string

--- SOS-Rest-Service/src/main/java/com/sagepay/sos/resources/TransactionFailureEndpoint.java
+++ SOS-Rest-Service/src/main/java/com/sagepay/sos/resources/TransactionFailureEndpoint.java
@@ -2,27 +2,45 @@ package com.sagepay.sos.resources;
 
 import com.sagepay.sos.core.Cancellation;
 import com.sagepay.sos.db.CancellationsDAO;
+import com.sagepay.sos.gateway.AESCryptoUtils;
 import org.joda.time.DateTime;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.QueryParam;
+import java.util.HashMap;
+import java.util.Map;
 
 @Path("/transaction-failure")
 public class TransactionFailureEndpoint {
 
     private final CancellationsDAO cancellationDAO;
 
+    private static final String HARDCODED_VENDOR_PASSWORD = "4X4CauKt56wYcM2M";
+
     public TransactionFailureEndpoint(CancellationsDAO cancellationsDAO) {
         this.cancellationDAO = cancellationsDAO;
     }
 
     @GET
-    public void registerFailedTransaction(@QueryParam("crypt") String cryptString, @QueryParam("customerMobileNumber") String customerMobileNumber, @QueryParam("customerEmailAddress") String customerEmailAddress){
-        //TODO: Decrypt cryptString.....for amount, vendorId, vendorName
-        registerCancellation(customerMobileNumber, customerEmailAddress, "vendorId", "vendorName");
+    public void registerFailedTransaction(@QueryParam("crypt") String cryptString, @QueryParam("customerMobileNumber") String customerMobileNumber, @QueryParam("customerEmailAddress") String customerEmailAddress) throws Exception {
+        String queryParams = AESCryptoUtils.decryptCryptString(cryptString, HARDCODED_VENDOR_PASSWORD);
+        Map<String, String> paramMap = getQueryParamMap(queryParams);
+        String vendorId = paramMap.getOrDefault("vendorId", "defaultVendorId");
+        String vendorName = paramMap.getOrDefault("vendorName", "defaultVendorName");
+        registerCancellation(customerMobileNumber, customerEmailAddress, vendorId, vendorName);
     }
 
+    private static Map<String, String> getQueryParamMap(String queryParams){
+        String[] params = queryParams.split("&");
+        Map<String, String> map = new HashMap<>();
+        for (String param : params){
+            String name = param.split("=")[0];
+            String value = param.split("=")[1];
+            map.put(name, value);
+        }
+        return map;
+    }
     private void registerCancellation(String customerMobileNumber, String customerEmailAddress, String vendorId, String vendorName) {
         Cancellation c = new Cancellation();
         c.setCustomerEmail(customerEmailAddress);
